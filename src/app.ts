c4a255e991a2b82fa8759b47b723d8a22c1940c3
Oliver Manzi <olivermanzi.dev@gmail.com>
2023-11-05 18:50:11 +0100

    (v0.3.4) patch: improves cors config

--- src/app.ts
+++ src/app.ts
@@ -2,19 +2,53 @@ import express from "express";
 import cors from "cors";
 import morgan from "morgan";
 import { router } from "./router";
+import { ALLOWED_ORIGINS } from "./config/env";
 
 const app = express();
 
-// middleware to enable cors
-app.use(cors());
+/**
+ * Configure cors middleware to curate requests based on origin
+ */
+app.use(cors({
+	/**
+   * This function is called for every request to check if the origin is allowed
+   * based on the ALLOWED_ORIGINS environment variable which can be set to a comma
+   * separated list of origins or to '*' to allow all origins.
+   * 
+   * A origin is allowed if:
+   * - ALLOWED_ORIGINS is undefined or '*'
+   * - ALLOWED_ORIGINS is a comma separated list and the origin is in the list
+   */
+	origin: (origin, callback) => {
+		// allow requests with no origin if ALLOWED_ORIGINS is undefined or '*'
+		if(!ALLOWED_ORIGINS || ALLOWED_ORIGINS === "*") {
+			return callback(null, true);
+		}
 
-// middleware to parse json
+		// allow requests with origin that matches ALLOWED_ORIGINS
+		else {
+			const allowedOrigins = ALLOWED_ORIGINS.split(",");
+			const isAllowed = allowedOrigins.includes(origin as string);
+			return callback(null, isAllowed);
+		}
+	},
+	methods: ["GET", "POST", "PUT", "DELETE"],
+	allowedHeaders: ["Content-Type", "Authorization"]
+}));
+
+/**
+ * Configure express to parse json body
+ */
 app.use(express.json());
 
-// middleware to log requests
+/**
+ * Configure morgan to log requests
+ */
 app.use(morgan("dev"));
 
-// configure routes
+/**
+ * Configure router middleware
+ */
 app.use(router);
 
 export default app;
--- src/config/env.ts
+++ src/config/env.ts
@@ -14,11 +14,25 @@ if (process.env.NODE_ENV === "testing") {
 const NODE_ENV = process.env.NODE_ENV || "production";
 const PORT = process.env.PORT || "3000";
 const DB_URI: string = process.env.DB_URI as string;
+const BUCKET_NAME: string = process.env.BUCKET_NAME as string;
+const BUCKET_S3_URI: string = process.env.BUCKET_URI as string;
+const BUCKET_S3_KEY: string = process.env.BUCKET_KEY as string;
+const BUCKET_S3_SECRET: string = process.env.BUCKET_SECRET as string;
+const BUCKET_GCP_ID: string = process.env.BUCKET_ID as string;
+const BUCKET_GCP_KEY_PATH: string = process.env.BUCKET_KEY_PATH as string;
+const ALLOWED_ORIGINS: string = process.env.ALLOWED_ORIGINS as string;
 const JWT_SECRET: string = process.env.JWT_SECRET as string;
 
 export {
 	NODE_ENV,
 	PORT,
 	DB_URI,
+	BUCKET_NAME,
+	BUCKET_S3_URI,
+	BUCKET_S3_KEY,
+	BUCKET_S3_SECRET,
+	BUCKET_GCP_ID,
+	BUCKET_GCP_KEY_PATH,
 	JWT_SECRET,
+	ALLOWED_ORIGINS
 };
