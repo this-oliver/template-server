6982e24cb948ebad58be4213675b4a172de3f96a
Oliver Manzi <olivermanzi.dev@gmail.com>
2023-12-10 16:31:42 +0100

    (v0.6.10) patch: removes data/middleware tests
    since this project is a boilerplate, it is inevitable that the logic in the middleware
    and data layers will changer over time hence it is not sustainable to maintain tests
    in these layers unlike utils/, for example, which won't change as much

--- src/utils/slug.test.ts
+++ src/utils/slug.test.ts
@@ -1,12 +1,37 @@
+import { expect } from 'chai';
+import { createSlug, isSlug } from './slug';
+
+
 describe('Slug Util', () => {
 	describe('createSlug()', () => {
-		it('should return a string');
-		it('should return a different string for the same input');
-		it('should throw an error if input is empty');
+		it('should return a string', () => {
+			const result = createSlug('sample');
+			expect(result).to.be.a('string');
+		});
+
+		it('should throw an error if input is empty', () => {
+			expect(() => createSlug('')).to.throw();
+			// eslint-disable-next-line @typescript-eslint/no-explicit-any
+			expect(() => createSlug(undefined as any)).to.throw();
+		});
 	});
 
 	describe('isSlug()', () => {
-		it('should return true if string is slug-like');
-		it('should return false if string is not slug-like');
+		it('should return true if string is slug-like', () => {
+			const validSlugs = ['sample', 'sample-slug', 'sample-slug-1', 'sample-slug-1-2'];
+			for(const slug of validSlugs){
+				const result = isSlug(slug);
+				expect(result).to.be.true;
+			}
+      
+		});
+
+		it('should return false if string is not slug-like', () => {
+			const invalidSlugs = ['sample slug', 'sample_slug', 'sample/slug', 'sample.slug'];
+			for(const slug of invalidSlugs){
+				const result = isSlug(slug);
+				expect(result).to.be.false;
+			}
+		});
 	});
 });
